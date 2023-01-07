930a86609e04233a500790ae39380e7030789fec
Nayan Kumar <naayaankumar@gmail.com>
2023-01-07 14:16:02 +0530

    updated the server

---
+++ react-json/index.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+server.use(middleware);
+server.use("", router);
+server.listen(8080, () => {
+  console.log("listening on http://localhost:8080/signup");
+});
+// // { "username": "nayan", "task": "reatc" },
+// { "username": "nayan", "task": "study" },
+// { "username": "nayan", "task": "novel" },
+// { "username": "kumar", "task": "reatc" },
+// { "username": "ajay", "task": "study" },
+// { "username": "raju", "task": "novel" }
