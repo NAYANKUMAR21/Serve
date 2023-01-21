9433299958a6924b6992c0a1697f4b42277950e4
Nayan Kumar <naayaankumar@gmail.com>
2023-01-21 15:36:54 +0530

    pushed changes

--- react_crud/src/features/auth/auth.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const file = {
-  name: { type: String },
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-};
-const userSchema = new mongoose.Schema(file);
-const userModel = mongoose.model("Auth", userSchema);
-module.exports = userModel;
