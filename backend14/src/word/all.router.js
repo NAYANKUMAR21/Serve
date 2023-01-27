1a718c1fdd164c196eec9d203d864255cbe6dfda
Nayan Kumar <naayaankumar@gmail.com>
2023-01-27 20:56:29 +0530

    comitted the sorting

--- backend14/src/word/all.router.js
+++ backend14/src/word/all.router.js
@@ -4,7 +4,7 @@ const app = express.Router();
 
 app.get("/", async (req, res) => {
   try {
-    const getPlayer = await wordModel.find().sort({ score: "asc" });
+    const getPlayer = await wordModel.find().sort({ score: "desc" });
     return res.status(200).send(getPlayer);
   } catch (er) {
     return res.status(404).send(er.message);
